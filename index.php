d463175d68ec85a5dad32c475d273b41112814fc
Along335 <131157841+Along335@users.noreply.github.com>
2023-10-27 14:03:44 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
 
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <section>
+        <p>
+            This is result from php function:
+        </p>
+    </section>
+    <footer>
+        <section id="copyright">
+            &copy This content is not copyrigth free<br>
+            <?php
+function currDateTime() {
+    date_default_timezone_set('Europe/Riga');
+    $currentTime = time();
+    $formattedDateTime = date('d-m-Y H:i:s', $currentTime);
+    return $formattedDateTime;
+}
+
+$date= currDateTime();
+echo "Current Date and Time: $date";
+?>
+        </section>
+    </footer>
+</body>
+</html>
